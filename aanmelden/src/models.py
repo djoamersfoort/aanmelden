50d54e7f129892b4cc907cf651401a5f4c0043ff
Ronald Moesbergen <github@rmoesbergen.nl>
2020-05-22 13:44:16 +0200

    Add Presence model

--- aanmelden/src/models.py
+++ aanmelden/src/models.py
@@ -3,5 +3,5 @@ from django.contrib.auth.models import User
 
 
 class Presence(models.Model):
-    user = models.OneToOneField(to=User, blank=False, null=False)
+    user = models.OneToOneField(to=User, blank=False, null=False, on_delete=models.CASCADE)
     date = models.DateField()
