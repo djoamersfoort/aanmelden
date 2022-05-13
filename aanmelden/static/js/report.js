dc7ed69198f6799bef61d4720770481fa8259ff4
Ronald Moesbergen <rmoesbergen@users.noreply.github.com>
2022-05-13 12:47:58 +0200

    fix: removed unused XMLHttpRequest

--- aanmelden/static/js/report.js
+++ aanmelden/static/js/report.js
@@ -3,7 +3,6 @@
 $(document).ready(function() {
     $("input").click(function(e) {
         let url = '/register/seen/' + $(this).attr('id') + '/' + $(this).is(":checked");
-        let xhttp = new XMLHttpRequest();
         fetch(url, { method: 'GET' })
             .catch(errorMsg => { console.log(errorMsg); });
     });
