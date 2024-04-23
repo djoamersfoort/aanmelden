f23197c2c6dfd00d76332b7ca6adf79d13a50380
Wiebe Beeftink <wipeautcrafter@gmail.com>
2024-04-23 22:59:31 +0200

    chore: clean up JS and CSS code

--- aanmelden/static/js/confetti.js
+++ aanmelden/static/js/confetti.js
@@ -1,37 +1,41 @@
 window.addEventListener('DOMContentLoaded', () => {
-    var count = 200;
-    var defaults = {
+    // defaults
+    const count = 200
+    const defaults = {
         origin: { y: 0.7 }
-    };
-    
-    function fire(particleRatio, opts) {
-        confetti({
-            ...defaults,
-            ...opts,
-            particleCount: Math.floor(count * particleRatio)
-        });
     }
     
+    const fire = (mult, opts) => confetti({
+        ...defaults,
+        ...opts,
+        particleCount: Math.floor(count * mult)
+    })
+    
+    // confetti bursts
     fire(0.25, {
         spread: 26,
         startVelocity: 55,
-    });
+    })
+
     fire(0.2, {
         spread: 60,
-    });
+    })
+
     fire(0.35, {
         spread: 100,
         decay: 0.91,
         scalar: 0.8
-    });
+    })
+
     fire(0.1, {
         spread: 120,
         startVelocity: 25,
         decay: 0.92,
         scalar: 1.2
-    });
+    })
+
     fire(0.1, {
         spread: 120,
         startVelocity: 45,
-    });
-});
+    })
+})
